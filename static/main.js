b2de161d3653ead2144fef82792b5bbafb1d0c68
arohiabhilasha <arohiabhilasha@gmail.com>
2021-06-18 10:11:04 +0530

    --- Auto-Git Commit ---

--- static/main.js
+++ static/main.js
@@ -19,3 +19,12 @@ $("nav a").each(function() {
 $("nav select").change(function() {
     window.location = $(this).find("option:selected").val();
   });
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
